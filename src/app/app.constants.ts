f5292d7aeded697fc60d40e8d58e7f822cfa7056
CodingProjects2030 <126919560+CodingProjects2030@users.noreply.github.com>
2023-07-28 02:57:30 +0300

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
 export const API_URL = "https://https://codingprojects2030.github.io/todo"
-export const TODO_JPA_API_URL = "https://https://codingprojects2030.github.io/todo/db.json"
+export const TODO_JPA_API_URL = "https://my-json-server.typicode.com/codingprojects2030/todo/jpa"
