10d32a6ef029705ae358229def0d7929ec673a0b
CodingProjects2030 <126919560+CodingProjects2030@users.noreply.github.com>
2023-07-28 03:13:59 +0300

    hgh

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://codingprojects2030.github.io/todo"
+export const API_URL = "https://my-json-server.typicode.com/codingprojects2030/todo"
 export const TODO_JPA_API_URL = "https://my-json-server.typicode.com/codingprojects2030/todo/jpa"
